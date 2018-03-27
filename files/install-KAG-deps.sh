a5a58913edb01bc0df1bc9cb4c261210dd6dda66
Dmitry Voronin <dimka665@gmail.com>
2018-03-27 06:01:44 +0400

    Move files

--- files/install-KAG-deps.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo aptitude install libfreetype6:i386
-sudo aptitude install libXxf86vm1:i386
-sudo aptitude install libx32stdc++6:i386
-sudo aptitude install libasound2-plugins:i386
--- testing.md
+++
@@ -1,5 +0,0 @@
-1. Download ISO  
-    http://cdimage.ubuntu.com/ubuntu-gnome/releases/
-
-1. Install in Virtual Box  
-    Turn on PAE/NX in machine settings if runs with error. 
